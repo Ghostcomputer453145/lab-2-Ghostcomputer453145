86fe8dc78d8db74849eb7c4548977b6198b066ef
Yumin <yjang2022@fau.edu>
2026-02-04 03:06:27 -0500

    Create the following nested elements in the <div className="App>"

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
       <div className="header">
         <h1>Samosa Selector</h1>
         <h2>Count:</h2>
-        <img className="samosa" src="https://static.vecteezy.com/system/resources/previews/024/589/992/original/samosa-transparent-background-free-png.png"/>
+        <img className="samosa" src="samosa.png"/>
       </div>
 
     </div>
